ecb1aed379f3da3bce8a5692708b780d1f8ea7fa
TheAndrey <me@theandrey.name>
2020-03-17 12:26:31 +0200

    First commit

---
+++ cli.php
@@ -0,0 +1,18 @@
+<?php
+
+define('ROOT_PATH', __DIR__);
+
+spl_autoload_register(function($class) {
+	$class = str_replace('\\', '/', $class);
+	$path = __DIR__ . '/classes/' . $class . '.php';
+	if(is_file($path)) {
+		/** @noinspection PhpIncludeInspection */
+		include_once $path;
+	}
+});
+
+$path = $_SERVER['argv'][1];
+$swagger = yaml_parse(file_get_contents($path));
+
+$generator = new SwaggerConverter();
+$generator->run($swagger);
